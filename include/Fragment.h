be0001c76825a384a56d90d95bb45e93777defbb
Alejandro Ortega <ort18248@uvg.edu.gt>
2023-09-14 18:38:07 -0600

    Added: Fragment stores world position

--- include/Fragment.h
+++ include/Fragment.h
@@ -11,14 +11,15 @@ struct Fragment
     float z;
     Color color;
     float intensity;
+    glm::vec3 worldPosition;
     glm::vec3 originalPosition;
 
     // Constructor
-    Fragment(float _x, float _y, float _z = 0.0f, Color _color = Color(255, 255, 255), float _intensity = 1.0f, const glm::vec3& _originalPosition = glm::vec3(0, 0, 0)) 
-        : x(_x), y(_y), z(_z), color(_color), intensity(_intensity), originalPosition(_originalPosition) {}
+    Fragment(float _x, float _y, float _z = 0.0f, Color _color = Color(255, 255, 255), float _intensity = 1.0f, const glm::vec3& _worldPosition = glm::vec3(0, 0, 0), const glm::vec3& _originalPosition = glm::vec3(0, 0, 0)) 
+        : x(_x), y(_y), z(_z), color(_color), intensity(_intensity), worldPosition(_worldPosition), originalPosition(_originalPosition) {}
 
-    Fragment(glm::vec3 position, Color _color = Color(255, 255, 255), float _intensity = 1.0f, const glm::vec3& _originalPosition = glm::vec3(0, 0, 0)) 
-        : x(position.x), y(position.y), z(position.z), color(_color), intensity(_intensity), originalPosition(_originalPosition) {}
+    Fragment(glm::vec3 position, Color _color = Color(255, 255, 255), float _intensity = 1.0f, const glm::vec3& _worldPosition = glm::vec3(0, 0, 0), const glm::vec3& _originalPosition = glm::vec3(0, 0, 0)) 
+        : x(position.x), y(position.y), z(position.z), color(_color), intensity(_intensity), worldPosition(_worldPosition), originalPosition(_originalPosition) {}
 
     // Overload the << operator for printing a Fragment
     friend std::ostream& operator<<(std::ostream& os, const Fragment& fragment) {
